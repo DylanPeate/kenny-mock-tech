d23f75dc016fdbd2a91a753453fa77bbb6689592
Dylan <d.peate63@gmail.com>
2022-10-27 11:39:30 -0400

    fixed css

--- src/components/todo/index.js
+++ src/components/todo/index.js
@@ -9,12 +9,6 @@ const Todo = () => {
     //https://jsonplaceholder.typicode.com/todos
 
     useEffect(() => {
-        // async function fetchTodos() {
-        //     const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-        //     const data = await response.json()
-        //     console.log(data, "<== Data")
-        //     return data
-        // }
 
         const fetchTodos = async () => {
             const response = await fetch('https://jsonplaceholder.typicode.com/todos')
